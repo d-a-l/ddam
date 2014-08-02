72141ff6a4a8b5c49b515f3be064f4be1522e24f
d-a-l <minombresbond@gmail.com>
2014-08-02 23:04:25 +0200

    relacionados templates and styles

---
+++ sites/all/themes/ddam/templates/node--fotografia--relacionados.tpl.php
@@ -0,0 +1,1 @@
+<?php include DRUPAL_ROOT . base_path() . path_to_theme() . "/templates/box-teaser.inc"; ?>
---
+++ sites/all/themes/ddam/templates/node--numero_publicacion_periodica--relacionados.tpl.php
@@ -0,0 +1,36 @@
+<div id="node-<?php print $node->nid; ?>" class="box-teaser col-lg-4 <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<?php print $user_picture; ?>
+
+   <div class="box-teaser-wrapper">
+
+      <?php print render($content['field_imagen']); ?>
+
+      <div class="box-teaser-body-top-wrapper">
+
+         <div class="node-type-icon"></div>
+
+         <div class="node-type">Revista</div>
+
+         <div class="content">
+          <?php
+            // We hide the comments and links now so that we can render them later.
+            print('<a href="">' . render($content['group_top']['field_publicacion_periodica']) . '</a>');
+            print render($content['group_top']);
+          ?>
+         </div>
+
+      </div>
+
+      <div class="box-teaser-body-bottom-wrapper">
+
+         <div class="content">
+          <?php
+            print render($content['group_bottom']);
+          ?>
+         </div>
+
+      </div>
+
+   </div>
+
+</div>
